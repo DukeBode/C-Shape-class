77223b8d0c3ec2e39def7a578a0e11c3e4a1062c
DukeBode <Duke123@aliyun.com>
2019-11-12 09:09:42 +0800

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyGenSpace
+{
+    class MyGen<T>
+    {
+        List<T> list;
+
+        void add<T>()
+        {
+
+        }
+        void Displist()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var typeInt = typeof(MyGen<int>);
+            var typeString = typeof(MyGen<string>);
+            var members = typeInt.GetMembers();
+            foreach (var member in members)
+            {
+                Console.WriteLine(member.Name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+2019-10-18
+
+编写控制台项目，声明一个MyGen<T> 泛型，包含一个List<T> 类型的字段list，设计一个向list 添加元素的add<T>方法和一个输出所有元素的Displist方法。在Main方法中，实例化为MyGen<int> 和MyGen<string>，采用反射技术显示该泛型的所有方法，并用相关设计进行测试。
